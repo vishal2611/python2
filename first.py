1088c675a94960b237edc33ab290442fa1df5c30
vishal2611 <vvselfwilled29@gmail.com>
2026-05-01 13:31:41 +0530

    recently uploaded

---
+++ first.py
@@ -0,0 +1,21 @@
+a=10  # Variable is a memory based location which holds values
+b=20
+c=a+b
+print(a)
+print(b)
+print(c)
+print(id(a)) # id is used for locating the address of variable
+
+
+num1=11
+num2=10
+sum1=num1+num2
+#print(num1,"+",num2,"=",sum1) #"" is used for message
+print(f"{num1} and {num2} is equal to {sum1}") # f is used for string formatting
+
+# I/O operation
+num1=int(input("Enter your number:"))
+num2=int(input("Enter your number:"))
+res=num1+num2
+print(res)
+
