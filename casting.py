563a1f97fb0fe84c2c9c2d5169d8d2de39d39d22
vishal2611 <vvselfwilled29@gmail.com>
2026-05-01 17:46:00 +0530

    New file updated

---
+++ casting.py
@@ -0,0 +1,23 @@
+#type casting --> in which we convert the data types & type conversion --> which gets converted automatically
+
+# num="100"
+# name = "Dev"
+# print(num+name)    #concatenate
+
+# print(type(name))
+
+# var1=float(1000)
+# print(type(var1))
+# print(var1)
+
+
+# num1="50"
+# num2="20"
+# res=int(num1)+int(num2)
+# print(res)
+
+
+num1=50
+num2=20
+res=str(num1)+str(num2)
+print(res)
