a02a7ef25644993bcb1155d7931ab6bc26adae85
vishal2611 <vvselfwilled29@gmail.com>
2026-05-06 10:31:41 +0530

    Updated06_05

---
+++ 06_may.py
@@ -0,0 +1,81 @@
+# identity opr
+
+
+
+
+
+
+
+
+#conditional statement
+# a=0
+# if a:
+#     print("Yes")
+# else:
+#     print("No")   # it return no because it is considered as false, if a=1 then it will return yes
+
+# a=2
+# if a:
+#     print("yes")
+# else:
+#     print("no")
+
+a=True
+b=False
+# print(a+b)   #in this case boolean gets converted into integer value when mathematical operation is applied(+,-m*,/)
+# print(a*b)
+# print(a==b)  #here it is considered as boolean
+
+
+# if 1*1*0:
+#     print("Yes")
+# else:
+#     print("No")
+
+#wap to check number given by user in odd or even
+# n1=int(input("Enter a number: "))
+# if n1%2==0:
+#     print(f"Number {n1} is even ")
+# else:
+#     print(f"Number {n1} is odd")
+
+
+#WAP to print the last digit of the number 456735
+n1=456735
+last_d1= n1%10
+last_d2= n1%100
+# print(f"Last digit of {n1} = {last_d1}")
+# print(f"Last 2 digit of {n1} = {last_d2}")
+
+#wap to find largest
+num1=3
+num2=9
+num3=12
+# if num1>num2 and num1>num3:
+#     print(f"Number1 {num1} is largest")
+# elif num2>num3 and num2>num1:
+#     print(f"Number2 {num2} is largest")
+# elif num3>num2 and num3>num1:
+#     print(f"Number3 {num3} is largest")
+# else:
+#     print("Error")
+
+#wap to find smallest
+num1=3
+num2=9
+num3=12
+# if num1<num2 and num1<num3:
+#     print(f"Number1 {num1} is smallest")
+# elif num2<num3 and num2<num1:
+#     print(f"Number2 {num2} is smallest")
+# elif num3<num2 and num3<num1:
+#     print(f"Number3 {num3} is smallest")
+# else:
+#     print("Error")
+
+
+#using indexing to find the last alphabet of word
+
+w1="Vishal"
+print(w1 [-1])
+
