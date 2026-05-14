5ef74db16a41c2ba4c4dac55273acea332fd2052
vishal2611 <vvselfwilled29@gmail.com>
2026-05-14 10:28:49 +0530

    New update 14-05

---
+++ 14_may.py
@@ -0,0 +1,52 @@
+#count space in the string
+# str1="This is python"
+# count=0
+# for i in str1:
+#     if i== " ":
+#         count+=1
+# print("Total space count: ",count)
+
+#count the number of characters excluding "o"
+# str2="How are you"
+# count=0
+# for i in str2:
+#     if i=="o":
+#         continue
+#     else:
+#         count+=1
+# print("Number of characters: ",count)
+
+# #string sanitization
+# address="D-1 267/268 MAYUR-VIHAR-PHASE-3 110096"
+# count=0
+# for i in address:
+#     if i in "0123456789":
+#         count+=1
+# print(count)
+
+
+
+#while loop
+    #initializer
+    # condition
+    # increment/decrement
+
+# i=1
+# while 1<=10:
+#     print(i)
+#     i+=1
+
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i-=1    #reverse printing
+
+#wap to print only even number from 10 to 20
+strt=10
+end=20
+while strt <= end:
+    if strt%2==0:
+        print(strt)
+    strt+=1
+
