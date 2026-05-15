08ed34761ae7de72fc70aa2c5c784d0284893d5e
vishal2611 <vvselfwilled29@gmail.com>
2026-05-15 10:25:31 +0530

    New Update 15-05

---
+++ 15_may.py
@@ -0,0 +1,24 @@
+#wap to print the total of even numbers from 1 to 15.
+# strt=1
+# end=15
+# sum=0
+# while strt<=end:
+#     if strt%2==0:
+#         sum+=strt
+#         print(sum)
+#     strt+=1
+
+#WAP to check the given string by user is pallindrom or not.
+str1=input("Enter the string : ")
+copy_t=str1
+rev=""
+i=len(str1) -1
+
+while i>=0:
+    rev+=str1[i]
+    i-=1
+if copy_t==rev:
+    print("Pallindrome")
+else:
+    print("Not Pallindrome")
+
